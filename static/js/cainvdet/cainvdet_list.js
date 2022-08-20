ebc0a2e76395bd7c44f8f0311af4da4faf7ab3f9
ismytv <ismytv@gmail.com>
2022-08-20 12:58:31 -0500

    timer a 3sec

--- static/js/cainvdet/cainvdet_list.js
+++ static/js/cainvdet/cainvdet_list.js
@@ -60,7 +60,7 @@ class FormApp extends Form {
             
             let oController = new Controller(`${controller_name}`, data, action_refresh);
             oController.fetch_action();
-        }, 10000)
+        }, 3000)
         
         this.init_modal_dialog();
         this.init_modal_cainvdetu();
